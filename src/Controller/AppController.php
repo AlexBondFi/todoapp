d178483bcd6f25c789040cccae2fc155291ad88a
MariiaSizova <sizovamaria24@gmail.com>
2023-05-12 12:20:39 +0300

    add controller for ToDo app

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,16 +3,54 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Task;
 
 class AppController extends AbstractController
 {
     #[Route('/app', name: 'app_app')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em)
     {
-        return $this->render('app/index.html.twig', [
-            'controller_name' => 'AppController',
-        ]);
+        $tasks = $em->getRepository(Task::class)->findBy([], ['id' => 'DESC']);
+        return $this->render('app/index.html.twig', ['tasks' => $tasks]);
+    }
+
+    #[Route('/create', name: 'create_task', methods: ['POST'])]
+    public function create(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $title = trim($request -> get('title'));
+        if(empty($title)) 
+            return $this->redirectToRoute('app_app');
+        $entityManager = $doctrine -> getManager();
+        $task = new Task;
+        $task->setTitle($title);
+        $entityManager->persist($task); 
+        $entityManager->flush(); 
+        return $this->redirectToRoute('app_app');
+    }
+
+    #[Route('/update/{id}', name: 'update_task')]
+    public function update($id, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $task = $entityManager->getRepository(Task::class)->find($id);
+        $task->setStatus(!$task->isStatus());
+        $entityManager->flush();
+        return $this->redirectToRoute('app_app');
+    }
+
+    #[Route('/delete/{id}', name: 'delete_task')]
+    public function delete($id, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $id = $entityManager->getRepository(Task::class)->find($id);
+        $entityManager->remove($id);
+        $entityManager->flush();
+        return $this->redirectToRoute('app_app');
     }
 }
+
